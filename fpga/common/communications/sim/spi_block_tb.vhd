60251e56851c6b74301ea7a6d95c208bf3eca724
Malcolm Nixon <malcolm.nixon@gmail.com>
2020-07-21 08:17:17 -0400

    Cleanup of SPI block port naming. Removed SPI block mode control - transfer-mode switching functionality belongs elsewhere.

--- fpga/common/communications/sim/spi_block_tb.vhd
+++ fpga/common/communications/sim/spi_block_tb.vhd
@@ -17,20 +17,19 @@ END ENTITY spi_block_tb;
 ARCHITECTURE tb OF spi_block_tb IS
 
     -- Clock period
-    CONSTANT clk_period : time := 100ns;
+    CONSTANT c_clk_period : time := 100ns;
     
     -- Signals to unit under test
-    SIGNAL clk     : std_logic;
-    SIGNAL rst     : std_logic;
-    SIGNAL mode    : std_logic_vector(1 DOWNTO 0);
-    SIGNAL cs      : std_logic;
-    SIGNAL sclk    : std_logic;
-    SIGNAL mosi    : std_logic;
-    SIGNAL miso    : std_logic;
-    SIGNAL rd_reg  : std_logic_vector(31 DOWNTO 0);
-    SIGNAL rd_strt : std_logic;
-    SIGNAL wr_reg  : std_logic_vector(31 DOWNTO 0);
-    SIGNAL wr_done : std_logic;    
+    SIGNAL mod_clk     : std_logic;
+    SIGNAL mod_rst     : std_logic;
+    SIGNAL spi_cs      : std_logic;
+    SIGNAL spi_sclk    : std_logic;
+    SIGNAL spi_mosi    : std_logic;
+    SIGNAL spi_miso    : std_logic;
+    SIGNAL dat_rd_reg  : std_logic_vector(31 DOWNTO 0);
+    SIGNAL dat_rd_strt : std_logic;
+    SIGNAL dat_wr_reg  : std_logic_vector(31 DOWNTO 0);
+    SIGNAL dat_wr_done : std_logic;    
     
     -- Test signals
     SIGNAL mosi_wr : std_logic_vector(31 DOWNTO 0);
@@ -41,17 +40,16 @@ BEGIN
     --! Instantiate spi_block as unit under test
     i_uut : ENTITY work.spi_block(rtl)
         PORT MAP (
-            clk_in      => clk,
-            rst_in      => rst,
-            mode_in     => mode,
-            cs_in       => cs,
-            sclk_in     => sclk,
-            mosi_in     => mosi,
-            miso_out    => miso,
-            rd_reg_in   => rd_reg,
-            rd_strt_out => rd_strt,
-            wr_reg_out  => wr_reg,
-            wr_done_out => wr_done
+            mod_clk_in      => mod_clk,
+            mod_rst_in      => mod_rst,
+            spi_cs_in       => spi_cs,
+            spi_sclk_in     => spi_sclk,
+            spi_mosi_in     => spi_mosi,
+            spi_miso_out    => spi_miso,
+            dat_rd_reg_in   => dat_rd_reg,
+            dat_rd_strt_out => dat_rd_strt,
+            dat_wr_reg_out  => dat_wr_reg,
+            dat_wr_done_out => dat_wr_done
         );
             
     --! @brief Clock generation process
@@ -59,12 +57,12 @@ BEGIN
     BEGIN
     
         -- Low for 1/2 clock
-        clk <= '0';
-        WAIT FOR clk_period / 2;
+        mod_clk <= '0';
+        WAIT FOR c_clk_period / 2;
         
         -- High for 1/2 clock
-        clk <= '1';
-        WAIT FOR clk_period / 2;
+        mod_clk <= '1';
+        WAIT FOR c_clk_period / 2;
         
     END PROCESS pr_clock;
     
@@ -73,69 +71,69 @@ BEGIN
     BEGIN
     
         -- Set SPI bus idle
-        cs   <= '1'; -- CS: high-idle
-        sclk <= '0'; -- SCLK: low-idle
-        mosi <= '0'; -- MOSI: idle
+        spi_cs   <= '1'; -- CS: high-idle
+        spi_sclk <= '0'; -- SCLK: low-idle
+        spi_mosi <= '0'; -- MOSI: idle
         
         -- Reset for 5 clocks
-        rst <= '1';
-        WAIT FOR clk_period * 5;
-        ASSERT (miso    = '0') REPORT "Expected miso low while in reset" SEVERITY warning;
-        ASSERT (rd_strt = '0') REPORT "Expected rd_strt low while in reset" SEVERITY warning;
-        ASSERT (wr_done = '0') REPORT "Expected wr_done low while in reset" SEVERITY warning;
+        mod_rst <= '1';
+        WAIT FOR c_clk_period * 5;
+        ASSERT (spi_miso    = '0') REPORT "Expected spi_miso low while in reset" SEVERITY warning;
+        ASSERT (dat_rd_strt = '0') REPORT "Expected dat_rd_strt low while in reset" SEVERITY warning;
+        ASSERT (dat_wr_done = '0') REPORT "Expected dat_wr_done low while in reset" SEVERITY warning;
         
         -- Release reset for 5 clocks
-        rst <= '0';
-        WAIT FOR clk_period * 5;
-        ASSERT (miso    = '0') REPORT "Expected miso low while idle" SEVERITY warning;
-        ASSERT (rd_strt = '0') REPORT "Expected rd_strt low while idle" SEVERITY warning;
-        ASSERT (wr_done = '0') REPORT "Expected wr_done low while idle" SEVERITY warning;
+        mod_rst <= '0';
+        WAIT FOR c_clk_period * 5;
+        ASSERT (spi_miso    = '0') REPORT "Expected spi_miso low while idle" SEVERITY warning;
+        ASSERT (dat_rd_strt = '0') REPORT "Expected dat_rd_strt low while idle" SEVERITY warning;
+        ASSERT (dat_wr_done = '0') REPORT "Expected dat_wr_done low while idle" SEVERITY warning;
         
         -- Start SPI transfer
-        cs <= '0';
-        WAIT FOR clk_period;
-        ASSERT (rd_strt = '1') REPORT "Expected rd_strt high for transfer start" SEVERITY warning;
+        spi_cs <= '0';
+        WAIT FOR c_clk_period;
+        ASSERT (dat_rd_strt = '1') REPORT "Expected dat_rd_strt high for transfer start" SEVERITY warning;
         
         -- Provide test pattern
-        rd_reg  <= X"AA550011";
-        mosi_wr <= X"DEADBEEF";
+        dat_rd_reg <= X"AA550011";
+        mosi_wr    <= X"DEADBEEF";
         
         -- Wait for 5 clocks (CS-to-Start)
-        WAIT FOR clk_period * 5;
-        ASSERT (rd_strt = '0') REPORT "Expected rd_strt low after transfer start" SEVERITY warning;
+        WAIT FOR c_clk_period * 5;
+        ASSERT (dat_rd_strt = '0') REPORT "Expected dat_rd_strt low after transfer start" SEVERITY warning;
         
         -- Clock SPI bus
         FOR i IN 0 TO 31 LOOP
             -- Drive MOSI
-            mosi <= mosi_wr(i);
+            spi_mosi <= mosi_wr(i);
             
             -- Wait for first-edge delay
-            WAIT FOR clk_period * 2;
+            WAIT FOR c_clk_period * 2;
             
             -- Read MISO and transition to second-edge
-            miso_rd(i) <= miso;
-            sclk       <= '1';
+            miso_rd(i) <= spi_miso;
+            spi_sclk   <= '1';
 
             -- Wait for second-edge delay
-            WAIT FOR clk_period * 2;
-            sclk <= '0';
+            WAIT FOR c_clk_period * 2;
+            spi_sclk <= '0';
         END LOOP;
 
         -- Wait for 5 clocks (End-to-CS)
-        WAIT FOR clk_period * 5;
+        WAIT FOR c_clk_period * 5;
         
         -- End SPI transfer
-        cs <= '1';
-        WAIT FOR clk_period;
-        ASSERT (wr_done = '1') REPORT "Expected wr_done high for transfer done" SEVERITY warning;
+        spi_cs <= '1';
+        WAIT FOR c_clk_period;
+        ASSERT (dat_wr_done = '1') REPORT "Expected dat_wr_done high for transfer done" SEVERITY warning;
     
         -- Wait for 5 clocks
-        WAIT FOR clk_period * 5;
-        ASSERT (wr_done = '0') REPORT "Expected wr_done low after transfer done" SEVERITY warning;
+        WAIT FOR c_clk_period * 5;
+        ASSERT (dat_wr_done = '0') REPORT "Expected dat_wr_done low after transfer done" SEVERITY warning;
         
         -- Verify test patterns
-        ASSERT (wr_reg = mosi_wr) REPORT "Expected wr_reg matches mosi_wr" SEVERITY warning;
-        ASSERT (miso_rd = rd_reg) REPORT "Expected miso_rd matches rd_reg" SEVERITY warning;
+        ASSERT (dat_wr_reg = mosi_wr) REPORT "Expected dat_wr_reg matches mosi_wr" SEVERITY warning;
+        ASSERT (miso_rd = dat_rd_reg) REPORT "Expected miso_rd matches dat_rd_reg" SEVERITY warning;
     
         -- Finish the simulation
         std.env.finish;
--- fpga/common/communications/source/spi_block.vhd
+++ fpga/common/communications/source/spi_block.vhd
@@ -10,19 +10,28 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 
 --! @brief SPI Block module
+--!
+--! This SPI block provides a simple 32-bit wide SPI shift register for 
+--! reading/writing data. At the start of a transfer when the CS goes low,
+--! the module drives the 'dat_rd_strt_out' signal for one clock to request
+--! new data be loaded into the 'dat_rd_reg_in' signal for transmitting.
+--! At the end of transfer when the CS goes high the module loads the received
+--! data into 'dat_wr_reg_out' then drives the 'dat_wr_done_out' signal for
+--! one clock indicating new data has been written to the SPI block.
+--!
+--! This module supports asynchronous reset through the mod_rst_in signal.
 ENTITY spi_block IS
     PORT (
-        clk_in      : IN    std_logic;                     --! Clock
-        rst_in      : IN    std_logic;                     --! Reset
-        mode_in     : IN    std_logic_vector(1 DOWNTO 0);  --! Mode select
-        cs_in       : IN    std_logic;                     --! SPI Chip-select
-        sclk_in     : IN    std_logic;                     --! SPI Clock
-        mosi_in     : IN    std_logic;                     --! SPI MOSI
-        miso_out    : OUT   std_logic;                     --! SPI MISO
-        rd_reg_in   : IN    std_logic_vector(31 DOWNTO 0); --! Read Register value
-        rd_strt_out : OUT   std_logic;                     --! Read Start flag
-        wr_reg_out  : OUT   std_logic_vector(31 DOWNTO 0); --! Write Register value 
-        wr_done_out : OUT   std_logic                      --! Write Done flag
+        mod_clk_in      : IN    std_logic;                     --! Module Clock
+        mod_rst_in      : IN    std_logic;                     --! Module Reset
+        spi_cs_in       : IN    std_logic;                     --! SPI Chip-select
+        spi_sclk_in     : IN    std_logic;                     --! SPI Clock
+        spi_mosi_in     : IN    std_logic;                     --! SPI MOSI
+        spi_miso_out    : OUT   std_logic;                     --! SPI MISO
+        dat_rd_reg_in   : IN    std_logic_vector(31 DOWNTO 0); --! Data Read Register value
+        dat_rd_strt_out : OUT   std_logic;                     --! Data Read Start flag
+        dat_wr_reg_out  : OUT   std_logic_vector(31 DOWNTO 0); --! Data Write Register value 
+        dat_wr_done_out : OUT   std_logic                      --! Data Write Done flag
     );
 END ENTITY spi_block;
 
@@ -47,20 +56,20 @@ ARCHITECTURE rtl OF spi_block IS
 BEGIN
 
     --! @brief SPI shift process
-    pr_shift : PROCESS (clk_in, rst_in) IS
+    pr_shift : PROCESS (mod_clk_in, mod_rst_in) IS
     BEGIN
     
-        IF (rst_in = '1') THEN
+        IF (mod_rst_in = '1') THEN
             -- Asynchronous reset of state
             state <= st_xfr_idle;
             shift <= (OTHERS => '0');
-        ELSIF (rising_edge(clk_in)) THEN
+        ELSIF (rising_edge(mod_clk_in)) THEN
         
             CASE state IS
             
                 WHEN st_xfr_idle =>
                     -- Detect CS-assert
-                    IF (cs_in = '0') THEN
+                    IF (spi_cs_in = '0') THEN
                         state <= st_xfr_strt;
                     ELSE
                         state <= st_xfr_idle;
@@ -68,16 +77,16 @@ BEGIN
                     
                 WHEN st_xfr_strt =>
                     -- Load read-register
-                    shift <= rd_reg_in;
+                    shift <= dat_rd_reg_in;
                     state <= st_clk_1st;
                     
                 WHEN st_clk_1st =>
                     -- Wait for CS-deassert or SCLK-first-edge
-                    IF (cs_in = '1') THEN
-                        wr_reg_out <= shift;                   -- Save write-register
-                        state      <= st_xfr_done;
-                    ELSIF (sclk_in = '1') THEN
-                        shift <= mosi_in & shift(31 DOWNTO 1); -- Save bit
+                    IF (spi_cs_in = '1') THEN
+                        dat_wr_reg_out <= shift;                   -- Save write-register
+                        state          <= st_xfr_done;
+                    ELSIF (spi_sclk_in = '1') THEN
+                        shift <= spi_mosi_in & shift(31 DOWNTO 1); -- Save bit
                         state <= st_clk_2nd;
                     ELSE
                         state <= st_clk_1st;
@@ -85,7 +94,7 @@ BEGIN
                     
                 WHEN st_clk_2nd =>
                     -- Wait for SCLK-second-edge
-                    IF (sclk_in = '0') THEN
+                    IF (spi_sclk_in = '0') THEN
                         state <= st_clk_1st;
                     ELSE
                         state <= st_clk_2nd;
@@ -105,14 +114,14 @@ BEGIN
     END PROCESS pr_shift;
 
     -- Always drive LSB of shift register to MISO
-    miso_out <= shift(0);
+    spi_miso_out <= shift(0);
     
     -- Trigger the read-start when in the transfer-start state
-    rd_strt_out <= '1' WHEN state = st_xfr_strt ELSE
-                   '0';
+    dat_rd_strt_out <= '1' WHEN state = st_xfr_strt ELSE
+                       '0';
     
     -- Trigger the write-done when in the transfer-done state
-    wr_done_out <= '1' WHEN state = st_xfr_done ELSE 
-                   '0';
+    dat_wr_done_out <= '1' WHEN state = st_xfr_done ELSE 
+                       '0';
 
 END ARCHITECTURE rtl;
