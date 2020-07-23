36eacbb89a38cb9904b2163b4f33ddf666168c2c
Malcolm Nixon <malcolm.nixon@gmail.com>
2020-07-22 20:24:36 -0400

    Added pwm_device entity and test bench.

---
+++ fpga/common/devices/sim/pwm_device_tb.vhd
@@ -0,0 +1,115 @@
+-------------------------------------------------------------------------------
+--! @file
+--! @brief PWM device test bench
+-------------------------------------------------------------------------------
+
+--! Using IEEE library
+LIBRARY ieee;
+
+--! Using IEEE standard logic components
+USE ieee.std_logic_1164.ALL;
+
+--! Using IEE standard numeric components
+USE ieee.numeric_std.ALL;
+
+--! @brief PWM device test bench
+ENTITY pwm_device_tb IS
+END ENTITY pwm_device_tb;
+
+--! Architecture tb of pwm_device_tb entity
+ARCHITECTURE tb OF pwm_device_tb IS
+
+    --! Test bench clock period
+    CONSTANT c_clk_period : time := 10ns;
+    
+    -- Signals to unit under test
+    SIGNAL clk         : std_logic;                     --! Clock input to pwm device
+    SIGNAL rst         : std_logic;                     --! Reset input to pwm device
+    SIGNAL adv         : std_logic;                     --! PWM advance input to pwm device
+    SIGNAL dat_wr_done : std_logic;                     --! Data write done input to pwm device
+    SIGNAL dat_wr_reg  : std_logic_vector(31 DOWNTO 0); --! Data write register input to pwm device
+    SIGNAL dat_rd_strt : std_logic;                     --! Data read start input to pwm device
+    SIGNAL dat_rd_reg  : std_logic_vector(31 DOWNTO 0); --! Data read register output from pwm device
+    SIGNAL pwm         : std_logic_vector(3 DOWNTO 0);  --! PWM outputs from pwm device
+
+BEGIN
+
+    --! Instantiate PWM device as unit under test
+    i_uut : ENTITY work.pwm_device(rtl)
+        PORT MAP (
+            clk_in         => clk,
+            rst_in         => rst,
+            adv_in         => adv,
+            dat_wr_done_in => dat_wr_done,
+            dat_wr_reg_in  => dat_wr_reg,
+            dat_rd_strt_in => dat_rd_strt,
+            dat_rd_reg_out => dat_rd_reg,
+            pwm_out        => pwm
+        );
+
+    --! @brief Clock generator process
+    --!
+    --! This generates the clk signal and the adv signal
+    pr_clock : PROCESS IS
+    BEGIN
+    
+        clk <= '0';
+        adv <= '0';
+        WAIT FOR c_clk_period / 2;
+
+        clk <= '1';
+        adv <= '1';
+        WAIT FOR c_clk_period / 2;
+        
+    END PROCESS pr_clock;
+    
+    --! @brief Stimulus process to drive PWM unit under test
+    pr_stimulus : PROCESS IS
+    BEGIN
+        
+        -- Reset inputs
+        dat_wr_reg  <= (OTHERS => '0');
+        dat_wr_done <= '0';
+        dat_rd_strt <= '0';
+        
+        -- Reset for 8 clock periods
+        rst <= '1';
+        WAIT FOR c_clk_period * 8;
+        
+        -- Take out of reset for 8 clock periods
+        rst <= '0';
+        WAIT FOR c_clk_period * 8;
+        
+        -- Read PWM device
+        dat_rd_strt <= '1';
+        WAIT FOR c_clk_period;
+        dat_rd_strt <= '0';
+        ASSERT (dat_rd_reg = B"00000000_00000000_00000000_00000000")
+            REPORT "Expected pwm_device zero duty cycles after reset"
+            SEVERITY warning;
+        
+        -- Write PWM command
+        dat_wr_reg  <= B"11111111_10101010_01010101_00000000";
+        dat_wr_done <= '1';
+        WAIT FOR c_clk_period;
+        dat_wr_done <= '0';
+        
+        -- Read PWM device
+        dat_rd_strt <= '1';
+        WAIT FOR c_clk_period;
+        dat_rd_strt <= '0';
+        ASSERT (dat_rd_reg = B"11111111_10101010_01010101_00000000") 
+            REPORT "Expected pwm_device non zero duty cycles after configuration"
+            SEVERITY warning;
+        
+        -- Take out of reset for 800 clock periods
+        rst <= '0';
+        WAIT FOR c_clk_period * 800;
+        
+        -- Finish the simulation
+        std.env.finish;
+        
+    END PROCESS pr_stimulus;
+    
+END ARCHITECTURE tb;
+
---
+++ fpga/common/devices/source/pwm_device.vhd
@@ -0,0 +1,94 @@
+-------------------------------------------------------------------------------
+--! @file
+--! @brief PWM device
+-------------------------------------------------------------------------------
+
+--! Using IEEE library
+LIBRARY ieee;
+
+--! Using IEEE standard logic components
+USE ieee.std_logic_1164.ALL;
+
+--! Using IEE standard numeric components
+USE ieee.numeric_std.ALL;
+
+--! @brief PWM device entity
+--!
+--! This entity manages four PWM devices.
+ENTITY pwm_device IS
+    PORT (
+        clk_in         : IN    std_logic;                     --! Module Clock
+        rst_in         : IN    std_logic;                     --! Module Reset (async)
+        adv_in         : IN    std_logic;                     --! PWM Advance flag
+        dat_wr_done_in : IN    std_logic;                     --! Device Write Done flag
+        dat_wr_reg_in  : IN    std_logic_vector(31 DOWNTO 0); --! Device Write Register value
+        dat_rd_strt_in : IN    std_logic;                     --! Device Read Start flag
+        dat_rd_reg_out : OUT   std_logic_vector(31 DOWNTO 0); --! Device Read Register value
+        pwm_out        : OUT   std_logic_vector(3 DOWNTO 0)   --! PWM outputs
+    );
+END ENTITY pwm_device;
+
+--! Architecture rtl of pwm_device entity
+ARCHITECTURE rtl OF pwm_device IS
+
+    --! Array type of four duty-cycles
+    TYPE duty_set IS ARRAY (3 DOWNTO 0) OF integer RANGE 0 TO 255;
+    
+    --! Duty cycles array
+    SIGNAL duty : duty_set;
+    
+BEGIN
+
+    --! Generate four PWMs
+    g_pwm : FOR i IN 0 TO 3 GENERATE
+
+        --! Generate PWM instance
+        i_pwm : ENTITY work.pwm(rtl)
+            GENERIC MAP (
+                count_max => 254
+            )
+            PORT MAP (
+                clk_in => clk_in,
+                rst_in => rst_in,
+                adv_in => adv_in,
+                duty_in => duty(i),
+                pwm_out => pwm_out(i)
+            );
+
+    END GENERATE g_pwm;
+
+    --! @brief Process to handle writes and resets
+    pr_write : PROCESS (clk_in, rst_in) IS
+    BEGIN
+        
+        IF (rst_in = '1') THEN
+            -- Reset duty cycles
+            duty(3) <= 0;
+            duty(2) <= 0;
+            duty(1) <= 0;
+            duty(0) <= 0;
+        ELSIF (rising_edge(clk_in) AND dat_wr_done_in = '1') THEN
+            -- Set duty cycles from write register
+            duty(3) <= to_integer(unsigned(dat_wr_reg_in(31 DOWNTO 24)));
+            duty(2) <= to_integer(unsigned(dat_wr_reg_in(23 DOWNTO 16)));
+            duty(1) <= to_integer(unsigned(dat_wr_reg_in(15 DOWNTO 8)));
+            duty(0) <= to_integer(unsigned(dat_wr_reg_in(7 DOWNTO 0)));
+        END IF;
+        
+    END PROCESS pr_write;
+
+    --! @brief Process to handle reads
+    pr_read : PROCESS (clk_in) IS
+    BEGIN
+    
+        IF (rising_edge(clk_in) AND dat_rd_strt_in = '1') THEN
+            -- Populate read register with duty cycles
+            dat_rd_reg_out <= std_logic_vector(to_unsigned(duty(3), 8)) &
+                              std_logic_vector(to_unsigned(duty(2), 8)) &
+                              std_logic_vector(to_unsigned(duty(1), 8)) &
+                              std_logic_vector(to_unsigned(duty(0), 8));
+        END IF;
+        
+    END PROCESS pr_read;
+    
+END ARCHITECTURE rtl;
